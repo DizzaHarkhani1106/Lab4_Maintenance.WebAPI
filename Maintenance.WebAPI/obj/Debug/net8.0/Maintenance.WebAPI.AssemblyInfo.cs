06702acd38443524094b4b4ee3be11c741f90aea
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-02-12 23:09:18 -0500

    Removed duplicate folder

--- Maintenance.WebAPI/obj/Debug/net8.0/Maintenance.WebAPI.AssemblyInfo.cs
+++ Maintenance.WebAPI/obj/Debug/net8.0/Maintenance.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maintenance.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf3391ffcca281b98971dca4389adf3da769f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maintenance.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maintenance.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
